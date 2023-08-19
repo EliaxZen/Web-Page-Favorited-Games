428d9e91227c8fb01f0465069878333ed24b3318
Elias Galvão Alves <132005740+EliaxZen@users.noreply.github.com>
2023-08-19 18:22:08 -0300

    upload arquivos 2

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,7 +10,22 @@ const getHours = () => {
   minutos.innerHTML = minutes;
   segundos.innerHTML = seconds;
 };
-
 setInterval(() => {
   getHours();
-}, 1);
+}, 100);
+function subirTela() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+function decidirBotaoScroll() {
+  if (window.scrollY === 0) {
+    //ocultar botão
+    document.querySelector(".scrollbutton").style.display = "none";
+  } else {
+    //mostrar botão
+    document.querySelector(".scrollbutton").style.display = "block";
+  }
+}
+window.addEventListener("scroll", decidirBotaoScroll);
